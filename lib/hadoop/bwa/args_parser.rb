f598bfc2f0b7508f9caa855f965998c18248bec7
WMW <wnameless@gmail.com>
2013-09-14 19:47:53 +0800

    Improve arguments parsing

--- lib/hadoop/bwa/args_parser.rb
+++ lib/hadoop/bwa/args_parser.rb
@@ -8,10 +8,18 @@ module Hadoop::Bwa
                    samse: [2], sampe: [5],    bwasw: [2] }.with_indifferent_access
     
     def parse(cmd)
-      files = cmd.strip.split(/\s+/).delete_if { |co| co =~ /^-/ }
-      cmd = files.shift
+      args = cmd.strip.split(/\s+/)
+      files = args.slice_before { |co| co =~ /^-/ }.to_a.last.delete_if { |co| co =~ /^-/ }
+      cmd = args.first
       if cmd !~ %r{#{CMD_FORMAT.keys.map { |c| "^#{c}$" }.join '|'}}
         raise InvalidCommandError, "Invalid command: #{cmd}."
+      end
+      files.delete cmd
+      while CMD_FORMAT[cmd].max < files.size
+        files.shift
+      end
+      if cmd == 'mem'
+        files.keep_if { |file| file =~ /^\w+(\.\w+)+$/ }
       end
       unless CMD_FORMAT[cmd].include? files.size
         raise RequiredFilesMissingError,
--- test/args_parser_test.rb
+++ test/args_parser_test.rb
@@ -6,7 +6,23 @@ class ArgsParserTest < Test::Unit::TestCase
   include Hadoop::Bwa::Errors
   
   def test_parse
-    assert_equal ['test.fa'], parse('index test.fa')
+    assert_equal ['test.1'], parse('index test.1')
+    assert_equal ['test.1', 'test.2'], parse('mem test.1 test.2')
+    assert_equal ['test.1', 'test.2', 'test.3'], parse('mem test.1 test.2 test.3')
+    assert_equal ['test.1', 'test.2'], parse('aln test.1 test.2')
+    assert_equal ['test.1', 'test.2'], parse('samse test.1 test.2')
+    assert_equal ['test.1', 'test.2', 'test.3', 'test.4', 'test.5'], parse('sampe test.1 test.2 test.3 test.4 test.5')
+    assert_equal ['test.1', 'test.2'], parse('bwasw test.1 test.2')
+  end
+  
+  def test_flag
+    assert_equal ['test.1'], parse('index -a -C sf test.1')
+    assert_equal ['test.1', 'test.2'], parse('mem -g -C sf test.1 test.2')
+    assert_equal ['test.1', 'test.2', 'test.3'], parse('mem -g -C sf test.1 test.2 test.3')
+    assert_equal ['test.1', 'test.2'], parse('aln -g -C sf test.1 test.2')
+    assert_equal ['test.1', 'test.2'], parse('samse -g -C sf test.1 test.2')
+    assert_equal ['test.1', 'test.2', 'test.3', 'test.4', 'test.5'], parse('sampe -g -C sf test.1 test.2 test.3 test.4 test.5')
+    assert_equal ['test.1', 'test.2'], parse('bwasw -g -C sf test.1 test.2')
   end
   
   def test_parse_error_1
